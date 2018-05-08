c88e5632ca04143de2fdd01307874f0055a8e432
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-05-08 19:19:42 -0400

    Fix security vulnerabities

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
+if ~/npm/bin/nsp check; then
 git add .
 git commit -m "$1"
 #git push origin master --tags
 npm version patch
 git push origin master --tags
 npm publish
+else
+	echo "Not publishing due to security vulnerabilites"
+fi
